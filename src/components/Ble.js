6bd10458553d6c60260390068b2493f2c906148c
SS80194 <ss80194@outlook.com>
2024-05-14 21:07:09 +0800

    Try to use ble-plx instead of ble-manager

--- src/components/Ble.js
+++ src/components/Ble.js
@@ -1,12 +1,25 @@
-import {BleManager} from 'react-native-ble-manager';
+import { BleManager } from 'react-native-ble-plx';
+
+const manager=new BleManager();
 
 export function initBLE()
 {
-  console.log("on running initBLE")
-  console.log("C");
-  BleManager.start({ showAlert: true }).then(() => {
-    // Success code
-    console.log("Module initialized");
-    }).catch((error)=>{console.log(error)})
-    
+  manager.startDeviceScan(null, null, (error, device) => {
+    if (error) {
+        console.error('扫描出错:', error);
+        return;
+    }
+
+    console.log('找到设备:', device.name, device.id);
+
+    // 连接设备
+    manager.connectToDevice(device.id)
+        .then((device) => {
+            console.log('已连接到设备:', device.name);
+            // 执行进一步的操作，如读取、写入、订阅特征等。
+        })
+        .catch((error) => {
+            console.error('连接出错:', error);
+        });
+  });
 }
